f35ec4eeb97f539a6910f6ec0f17786716e6cb19
Brian <fogg4444@gmail.com>
2015-12-29 18:04:25 -0800

    implemented jquery auto resizing
    Used the jquery library to implement a resizing function. Keeps 'body' always at 100% height and width

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
-$(document).ready(){
-	console.log('test ready');
-}
+$(document).ready(function(){
+	console.log('jquery test');
+
+	var resizeBody = function() {
+		console.log('resized width');
+		// console.log(window.innerHeight);
+		var height = window.innerHeight;
+		var width = window.innerWidth;
+
+		$(document.body).css("height", height);
+		console.log(document.body);
+	}
+
+	resizeBody();
+
+	$( window ).resize(function(){
+		resizeBody();
+	});
+
+});
